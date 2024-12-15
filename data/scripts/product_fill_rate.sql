2058594d6dc2eba221a91b2099ff57599b1cbaa0
VincentCYS <vincent.chengyunsheng@gmail.com>
2024-12-15 22:36:21 +0800

    updated to calculate based on calculatedinventory

--- data/scripts/product_fill_rate.sql
+++ data/scripts/product_fill_rate.sql
@@ -9,24 +9,50 @@ WITH DailyDemand AS (
   LEFT JOIN "SalesOrderDetail" tb2
     ON tb1.SalesOrderId = tb2.SalesOrderId
   GROUP BY tb1.OrderDate, tb2.ProductID
+),
+
+LatestInventory AS (
+  SELECT 
+    di.ProductID,
+    di.TransactionDate,
+    di.CalculatedInventory,
+    ROW_NUMBER() OVER (
+      PARTITION BY di.ProductID
+      ORDER BY di.TransactionDate DESC
+    ) AS Rank
+  FROM DailyInventoryLevels di
+  WHERE di.TransactionDate <= (
+    SELECT OrderDate FROM DailyDemand dd WHERE di.ProductID = dd.ProductID
+  )
+),
+
+DemandAndInventory AS (
+  SELECT 
+    dd.OrderDate,
+    dd.ProductID,
+    dd.Demand,
+    COALESCE(
+        (SELECT CalculatedInventory 
+        FROM LatestInventory li 
+        WHERE dd.ProductID = li.ProductID 
+          AND li.TransactionDate <= dd.OrderDate
+        ORDER BY li.TransactionDate DESC
+        LIMIT 1),
+        0
+    ) AS CalculatedInventory
+  FROM DailyDemand dd
 )
 
-SELECT 
-  tb1.OrderDate,
-  tb1.ProductID,
-  tb1.Demand,
-  COALESCE(SUM(tb2.Quantity), 0) AS CumulativeInventory,
+SELECT
+  OrderDate,
+  ProductID,
+  Demand,
+  CalculatedInventory,
   CASE 
-    WHEN COALESCE(SUM(tb2.Quantity), 0) = 0 THEN 0.00
-    ELSE CAST(tb1.Demand AS REAL) / SUM(tb2.Quantity)
+    WHEN COALESCE(CalculatedInventory , 0) = 0 THEN 0.00
+    ELSE CAST(Demand AS REAL) / CalculatedInventory
   END AS FillRate
-FROM DailyDemand tb1
-LEFT JOIN ProductInventory tb2
-  ON tb1.ProductID = tb2.ProductID
-GROUP BY 
-  tb1.OrderDate,
-  tb1.ProductID,
-  tb1.Demand
-ORDER BY tb1.OrderDate, tb1.ProductID
+FROM DemandAndInventory
+ORDER BY OrderDate, ProductID
 
 
