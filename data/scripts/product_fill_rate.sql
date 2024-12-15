9ec54084c208246bb4e022f9875fcebaee4a7607
VincentCYS <vincent.chengyunsheng@gmail.com>
2024-12-15 17:17:08 +0800

    updated to use product inventory

--- data/scripts/product_fill_rate.sql
+++ data/scripts/product_fill_rate.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS ProductFillRate;
 CREATE TABLE ProductFillRate AS
 WITH DailyDemand AS (
   SELECT 
@@ -14,16 +15,18 @@ SELECT
   tb1.OrderDate,
   tb1.ProductID,
   tb1.Demand,
-  COALESCE(tb2.CumulativeInventory, 0) AS CumulativeInventory,
+  COALESCE(SUM(tb2.Quantity), 0) AS CumulativeInventory,
   CASE 
-    WHEN tb2.CumulativeInventory IS NULL OR tb2.CumulativeInventory = 0 THEN 0.00
-    ELSE CAST(tb1.Demand AS REAL) / tb2.CumulativeInventory
+    WHEN COALESCE(SUM(tb2.Quantity), 0) = 0 THEN 0.00
+    ELSE CAST(tb1.Demand AS REAL) / SUM(tb2.Quantity)
   END AS FillRate
 FROM DailyDemand tb1
-LEFT JOIN DailyInventoryLevels tb2
-  ON 
-    tb1.ProductID = tb2.ProductID
-    AND tb1.OrderDate = tb2.InventoryDate
+LEFT JOIN ProductInventory tb2
+  ON tb1.ProductID = tb2.ProductID
+GROUP BY 
+  tb1.OrderDate,
+  tb1.ProductID,
+  tb1.Demand
 ORDER BY tb1.OrderDate, tb1.ProductID
 
 
