2a24c7d1b00a54619b13fccc6159cdaff9572430
VincentCYS <vincent.chengyunsheng@gmail.com>
2024-12-16 19:33:04 +0800

    added product ytd fill rate bar chart

--- components/bar_charts.py
+++ components/bar_charts.py
@@ -23,3 +23,27 @@ def order_fill_weekly_chart(conn):
         x_label="Week Number", 
         y_label="Order Fill Rate (%)"
     )
+
+def product_ytd_fill_rate(conn):
+    df = pd.read_sql(
+        """
+            SELECT 
+                strftime('%Y', OrderDate) AS Year, 
+                ProductID,
+                ROUND(CAST(SUM(Demand) AS FLOAT) / SUM(CalculatedInventory), 2) * 100 AS ProductFillRate
+            FROM ProductFillRate
+            WHERE 
+                strftime('%Y', OrderDate) = ?
+            GROUP BY Year, ProductID
+            HAVING ProductFillRate <= 100
+            ORDER BY ProductFillRate DESC
+            LIMIT 10
+        """
+        , conn, params=(st.session_state['year'],))
+    st.text("Product YTD Fill Rate")
+    st.bar_chart(
+        df, 
+        x="ProductID", 
+        y="ProductFillRate", 
+        horizontal=True
+    )
--- pages/inventory_management.py
+++ pages/inventory_management.py
@@ -32,3 +32,5 @@ with warnings.catch_warnings():
     if st.session_state['product_number']:
         st.subheader(st.session_state['product_number'] + " Daily Inventory Levels")
         product_daily_inventory_levels_chart(conn)
+
+    product_ytd_fill_rate(conn)
