8c7ac4c024a950e319dd3abb3120aa09e36be39b
cyshen11 <cyshen11@gmail.com>
2024-12-17 11:20:54 +0800

    replaced by generate_table script

--- data/scripts/python/generate_inventory_table.py
+++
@@ -1,3 +0,0 @@
-import tablefaker
-
-tablefaker.to_csv("data/scripts/yaml/inventory.yaml", "data/csv/inventory.csv")
--- data/scripts/python/generate_product_table.py
+++
@@ -1,3 +0,0 @@
-import tablefaker
-
-tablefaker.to_csv("data/scripts/yaml/products.yaml", "data/csv/products.csv")
