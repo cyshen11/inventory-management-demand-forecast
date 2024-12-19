9f3eb2b8a989d5ea9e23d5f26efadae121274bc6
VincentCYS <vincent.chengyunsheng@gmail.com>
2024-12-19 22:37:37 +0800

    added to calculate orders from order_items

---
+++ data/scripts/python/calculate_orders.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import random
+import datetime
+
+df_orders_items = pd.read_csv('data/csv/order_items.csv')
+
+df_orders_items['is_fulfilled'] = df_orders_items['quantity_fulfilled'] == df_orders_items['quantity_ordered']
+
+df_orders = df_orders_items.groupby('order_id').agg({
+    'is_fulfilled': 'sum',
+}).reset_index()
+
+df_orders.index.name = 'id'
+
+df_orders['customer_id'] = [random.randint(1, 1000) for _ in range(len(df_orders))]
+
+df_orders['order_date'] = [datetime.date.today() - datetime.timedelta(days=random.randint(0, 365)) for _ in range(len(df_orders))]
+
+df_orders.to_csv('data/csv/orders.csv')
