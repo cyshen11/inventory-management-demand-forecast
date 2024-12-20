95ffa86df43001e47c432108764e647d840bfd8e
VincentCYS <vincent.chengyunsheng@gmail.com>
2024-12-20 23:02:49 +0800

    updated to filter based on selected year

--- components/bar_charts.py
+++ components/bar_charts.py
@@ -3,7 +3,11 @@ import pandas as pd
 
 def order_fill_weekly_chart():
     df_input = pd.read_csv("data/csv/orders.csv")
+    
     df_input['order_date'] = pd.to_datetime(df_input['order_date'])
+    df_input['year'] = df_input['order_date'].dt.year
+    df_input = df_input.loc[df_input['year'] == st.session_state['year']]
+
     df_output = df_input.groupby(df_input['order_date'].dt.isocalendar().week).agg({'is_fulfilled': 'sum', 'order_id': 'count'}).reset_index()
     df_output.columns = ['week', 'orders_fulfilled', 'total_orders']
     df_output['order_fill_rate'] = round(df_output['orders_fulfilled'] / df_output['total_orders'] * 100, 2)
