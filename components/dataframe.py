73d6b10057e7d378358e444ce90ba89a54055b53
VincentCYS <vincent.chengyunsheng@gmail.com>
2024-12-21 16:02:38 +0800

    updated to show product number

--- components/dataframe.py
+++ components/dataframe.py
@@ -44,13 +44,13 @@ def dataframe_products_not_filled(order_id):
     product_df = pd.read_csv("data/csv/products.csv")
     df = pd.merge(order_items_df, product_df, on='product_id', how='left')
     
-    df = df[["order_id", "product_id", "product_name", "quantity_ordered", "quantity_fulfilled", "pct_fulfilled"]]
+    df = df[["order_id", "product_number", "quantity_ordered", "quantity_fulfilled", "pct_fulfilled"]]
     
-    df = df.sort_values(by=["pct_fulfilled", "product_name"])
+    df = df.sort_values(by=["pct_fulfilled", "product_number"])
 
-    df.columns = ["Order ID", "Product ID", "Product Name", "Quantity Ordered", "Quantity Fulfilled", "% Fulfilled"]
+    df.columns = ["Order ID", "Product Number", "Quantity Ordered", "Quantity Fulfilled", "% Fulfilled"]
 
-    df_display = df[["Product Name", "Quantity Ordered", "Quantity Fulfilled", "% Fulfilled"]]
+    df_display = df[["Product Number", "Quantity Ordered", "Quantity Fulfilled", "% Fulfilled"]]
 
     def color_coding(row):
         return ['background-color:rgba(255, 75, 75, 0.1)'] * len(
