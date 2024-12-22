3af7a1e82e8a6492e29b904a06cfcf2fe5824d9d
VincentCYS <vincent.chengyunsheng@gmail.com>
2024-12-22 15:59:59 +0800

    corrected the display formula for ROP

--- components/rop.py
+++ components/rop.py
@@ -19,7 +19,7 @@ def rop():
 
   st.info(f"""
     Reorder Point
-          = AS x Lead Time + 
+          = AS x Lead Time 
           = {avg_daily_sales} x {delivery_lead_time} + {safety_stock} 
           = **{ROP}**
   """)
