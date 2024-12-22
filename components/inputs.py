ee7a445df5ac40a526b23601607ed6f7a3db86fc
VincentCYS <vincent.chengyunsheng@gmail.com>
2024-12-22 20:57:00 +0800

    added input targeted product fill rate

--- components/inputs.py
+++ components/inputs.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-def input_service_level():
-    st.number_input("Specify Service Level", value=0.95, step=0.05)
+def input_product_fill_rate(col):
+    col.number_input("Specify Targeted Product Fill Rate", value=0.90, step=0.01)
--- components/ss_norm.py
+++ components/ss_norm.py
@@ -1,7 +1,9 @@
 from components.filters import *
+from components.inputs import *
 
 @st.fragment
 def ss_norm():
-  col1, col2 = st.columns(2)
+  col1, col2, col3 = st.columns(3)
   selectbox_uncertainty_type(col1)
   selectbox_time_units(col2)
+  input_product_fill_rate(col3)
