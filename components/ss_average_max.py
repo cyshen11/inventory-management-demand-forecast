56135d7069a6681a020e60e5357a69844c97203a
VincentCYS <vincent.chengyunsheng@gmail.com>
2024-12-29 17:08:15 +0800

    updated number input labels

--- components/ss_average_max.py
+++ components/ss_average_max.py
@@ -5,10 +5,10 @@ def ss_average_max():
   col1, col2 = st.columns(2)
   col3, col4 = st.columns(2)
 
-  max_lead_time = col1.number_input("Max Lead Time (days)", value=20)
-  average_lead_time = col2.number_input("Average Lead Time (days)", value=10)
-  max_sales = col3.number_input("Max Sales per day", value=st.session_state["max_demand"])
-  avg_sales = col4.number_input("Average Sales per day", value=st.session_state["avg_demand"])
+  max_lead_time = col1.number_input("Specify Max Lead Time (days)", value=20)
+  average_lead_time = col2.number_input("Specify Average Lead Time (days)", value=10)
+  max_sales = col3.number_input("Specify Max Sales per day", value=st.session_state["max_demand"])
+  avg_sales = col4.number_input("Specify Average Sales per day", value=st.session_state["avg_demand"])
 
   ss = round((max_lead_time * max_sales) - (average_lead_time * avg_sales))
   rop = round(ss + (average_lead_time * avg_sales))
