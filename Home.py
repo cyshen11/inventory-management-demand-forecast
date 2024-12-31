775e10c93903d93cbae7ed4663b0f275c14b4951
VincentCYS <vincent.chengyunsheng@gmail.com>
2024-12-31 12:54:40 +0800

    updated components import

--- Home.py
+++ Home.py
@@ -1,23 +1,22 @@
 import streamlit as st
-from components.filters import *
-from components.metrics import *
-from components.bar_charts import *
-from components.dataframe import *
-from components.line_charts import *
-from components.inputs import *
-from components.dataset import *
-from components.ss_basic import *
-from components.ss_average_max import ss_average_max
-from components.ss_norm import *
-from components.simulation import *
-from components.sidebar import sidebar
+from components import (
+    bar_charts,
+    line_charts,
+    dataset,
+    ss_basic,
+    ss_norm,
+    simulation,
+    ss_average_max,
+    sidebar,
+    dataset
+)
 import warnings
 warnings.filterwarnings("ignore")
 
 def main():
     st.title("Inventory Optimization & Demand Forecast")
 
-    dynamic_filters, filtered_data = sidebar()
+    dynamic_filters, filtered_data = sidebar.sidebar()
     filters = st.session_state[dynamic_filters.filters_name]
 
     if len(filters['Product_Code']) > 0 and len(filters['Year']) > 0:
@@ -25,13 +24,13 @@ def main():
         st.session_state['year'] = filters['Year'][0]
         st.session_state['product_code'] = filters['Product_Code'][0]
 
-        product_daily_inventory_levels_chart(filtered_data)
+        line_charts.product_daily_inventory_levels_chart(filtered_data)
 
-        lead_time_data = DatasetLeadTime(filters).data
-        lead_time_chart(lead_time_data)
+        lead_time_data = dataset.DatasetLeadTime(filters).data
+        bar_charts.lead_time_chart(lead_time_data)
 
         with st.expander("Calculate Economic Order Quantity (EOQ)"):
-            eoq()
+            ss_basic.eoq()
 
         with st.expander("Calculate Safety Stock and Reorder Point"):
             tab1, tab2, tab3, tab4, tab5 = st.tabs([
@@ -43,22 +42,22 @@ def main():
             ])
 
             with tab1:
-                ss_basic()
+                ss_basic.ss_basic()
 
             with tab2:
-                ss_average_max()
+                ss_average_max.ss_average_max()
 
             with tab3:
-                ss_cycle_service_rate(filtered_data, lead_time_data)
+                ss_norm.ss_cycle_service_rate(filtered_data, lead_time_data)
 
             with tab4:
-                ss_fill_rate(filtered_data, lead_time_data)
+                ss_norm.ss_fill_rate(filtered_data, lead_time_data)
             
             with tab5:
-                ss_holding_stockout(filtered_data, lead_time_data)
+                ss_norm.ss_holding_stockout(filtered_data, lead_time_data)
         
         with st.expander("Simulation"):
-            simulation(lead_time_data)
+            simulation.simulation(lead_time_data)
 
 if __name__ == "__main__":
     main()
