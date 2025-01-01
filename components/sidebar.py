211e08aef1dc74799dc2904a97f6da502184fc66
VincentCYS <vincent.chengyunsheng@gmail.com>
2025-01-01 10:05:06 +0800

    updated version and cd workflow

--- components/sidebar.py
+++ components/sidebar.py
@@ -79,7 +79,7 @@ def sidebar():
                 delete_uploaded_data()
                 st.markdown("✅ Deleted successfully!")
 
-        st.sidebar.markdown("v1.0.0")
+        st.sidebar.markdown("v1.0.1")
 
     return dynamic_filters, filtered_data
 
